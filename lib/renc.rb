94e130d509172046e920244d51f6fe2e540e2bef
k-ta-yamada <key.luvless@gmail.com>
2016-02-26 22:28:23 +0900

    [add][mod] v0.2.0 base features

--- lib/renc.rb
+++ lib/renc.rb
@@ -1,12 +1,26 @@
-require "renc/version"
+require 'renc/version'
 
+# namespace
 module Renc
-  def self.enc(val, encoding = Encoding::UTF_8)
-    case val
-    when :dummy
-      # do
-    else
-      val
+  def self.enc(obj, encoding = Encoding::UTF_8)
+    case obj
+    when String then obj.encode(encoding)
+    when Hash   then enc_hash(obj, encoding)
+    when Array  then enc_array(obj, encoding)
+    else             obj
     end
+  end
+
+  # private
+
+  def self.enc_hash(obj, encoding)
+    obj.each_with_object({}) do |args, h|
+      key, val = args
+      h[key] = enc(val, encoding)
+    end
+  end
+
+  def self.enc_array(obj, encoding)
+    obj.map { |val| enc(val, encoding) }
   end
 end
--- spec/renc_spec.rb
+++ spec/renc_spec.rb
@@ -5,11 +5,29 @@ describe Renc do
     expect(Renc::VERSION).not_to be nil
   end
 
+  let(:encoding) { Encoding::ASCII }
+
   describe '.enc' do
+    context 'String value' do
+      it 'encode value' do
+        encoded = described_class.enc('abc', encoding)
+        expect(encoded.encoding).to eq(encoding)
+      end
+    end
+
     context 'Hash value is String' do
-      xit 'encode value' do
-        encoded = described_class.enc({ a: 1, b: 'abc'}, Encoding::ASCII)
-        expect(encoded[:b].encoding).to eq(Encoding::ASCII)
+      it 'encode value' do
+        encoded = described_class.enc({ a: 1, b: 'abc' }, encoding)
+        expect(encoded[:a]).to eq(1)
+        expect(encoded[:b].encoding).to eq(encoding)
+      end
+    end
+
+    context 'Array value is String' do
+      it 'encode value' do
+        encoded = described_class.enc([1, 2, 3, :a, 'b'], encoding)
+        expect(encoded.first).to eq(1)
+        expect(encoded.last.encoding).to eq(encoding)
       end
     end
   end
