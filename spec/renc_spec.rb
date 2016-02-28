7a6f2f50960b76f823fb56762d1b265b9648cffb
k-ta-yamada <key.luvless@gmail.com>
2016-02-28 23:39:04 +0900

    [wip]

--- spec/renc_spec.rb
+++ spec/renc_spec.rb
@@ -5,24 +5,29 @@ describe Renc do
 
   shared_examples 'example' do |obj|
     context obj.class do
-      let(:encoding) { Encoding::ASCII }
+      let(:encoding) { Encoding::Windows_31J }
       subject { renc(obj, encoding) }
       it { is_expected.to eq(obj) }
       it 'all String value is encoded' do
-        expected = subject.is_a?(Hash) ? subject.values : Array(subject)
+        expected = subject.is_a?(Hash) ? subject.values : [subject]
+        expected.flatten!
         expected.select! { |v| v.is_a?(String) }
         expect(expected.map(&:encoding)).to all(eq(encoding))
       end
     end
   end
 
-  describe '.renc' do
+  describe '#renc' do
     # String
     it_behaves_like 'example', 'abcd'
 
     # Hash
     it_behaves_like 'example', a: 'a', b: 'b'
-    it_behaves_like 'example', a: 'a', b: 1, c: { ca: 'abc' }
+    it_behaves_like 'example', a: { aa: { aaa: { aaaa: 'abc' } } }
+    it_behaves_like 'example', a: 'a',
+                               b: 1,
+                               c: { ca: 'abc', cb: 123 },
+                               d: [1, 2, 3, 'abc', { da: 123, db: 'abc' }]
 
     # Array
     it_behaves_like 'example', %w(a b c)
